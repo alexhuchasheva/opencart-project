c4fab973359ad26ef2f084a12bc8a5fe9827b923
Alex Huchasheva; <alexhuchasheva@gmail.com;>
2018-08-31 17:32:44 +0300

    Opencart Downloaded

---
+++ opencart/public_html/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.createasite.ru/admin/');
+define('HTTP_CATALOG', 'http://opencart.createasite.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.createasite.ru/admin/');
+define('HTTPS_CATALOG', 'http://opencart.createasite.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/h/huchasheva/opencart/public_html/admin/');
+define('DIR_SYSTEM', '/home/h/huchasheva/opencart/public_html/system/');
+define('DIR_IMAGE', '/home/h/huchasheva/opencart/public_html/image/');
+define('DIR_LANGUAGE', '/home/h/huchasheva/opencart/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/h/huchasheva/opencart/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/h/huchasheva/opencart/public_html/system/config/');
+define('DIR_CACHE', '/home/h/huchasheva/opencart/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/h/huchasheva/opencart/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/h/huchasheva/opencart/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/h/huchasheva/opencart/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/h/huchasheva/opencart/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/h/huchasheva/opencart/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'huchasheva_opc');
+define('DB_PASSWORD', 'Z1X4v8TD');
+define('DB_DATABASE', 'huchasheva_opc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ opencart/public_html/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.createasite.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.createasite.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/h/huchasheva/opencart/public_html/catalog/');
+define('DIR_SYSTEM', '/home/h/huchasheva/opencart/public_html/system/');
+define('DIR_IMAGE', '/home/h/huchasheva/opencart/public_html/image/');
+define('DIR_LANGUAGE', '/home/h/huchasheva/opencart/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/h/huchasheva/opencart/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/h/huchasheva/opencart/public_html/system/config/');
+define('DIR_CACHE', '/home/h/huchasheva/opencart/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/h/huchasheva/opencart/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/h/huchasheva/opencart/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/h/huchasheva/opencart/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/h/huchasheva/opencart/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'huchasheva_opc');
+define('DB_PASSWORD', 'Z1X4v8TD');
+define('DB_DATABASE', 'huchasheva_opc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
